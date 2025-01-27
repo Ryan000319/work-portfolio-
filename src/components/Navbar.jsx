7cc2b91a269f8d2a62d6654e87002e83e75e8c91
Ryan000319 <ryanalistairallen14@gmail.com>
2025-01-28 02:38:43 +0800

    navigation bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,42 @@
-import React from "react";
+import React, { useState, useEffect} from "react";
 import "../styles/navbar.css";
 
 const Navbar = () => {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+  const [isShrunk, setIsShrunk] = useState(false);
+
+  // Toggle Mobile Menu
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
+  useEffect(() => { 
+    const handleScroll = () => {
+      if (window.scrollY > 20) {
+        setIsShrunk(true);
+      } else {
+        setIsShrunk(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <nav className="navbar">
+    <nav className={`navbar ${isShrunk ? "shrink" : ""}`}>  
       <div className="navbar-container">
-        <h1>My Portfolio</h1>
-        <div>
-          <a href="#about">About</a>
-          <a href="#projects">Projects</a>
-          <a href="#contact">Contact</a>
+        <h1 className="logo">My Portfolio</h1>
+        <div className="hamburger" onClick={toggleMobileMenu}>
+          ☰
+        </div>
+        <div className={`navbar-links ${isMobileMenuOpen ? "active" : ""}`}>
+          <a href="#about" onClick={() => setIsMobileMenuOpen(false)}>About</a>
+          <a href="#projects" onClick={() => setIsMobileMenuOpen(false)}>Projects</a>
+          <a href="#contact" onClick={() => setIsMobileMenuOpen(false)}>Contact</a>
         </div>
       </div>
     </nav>
